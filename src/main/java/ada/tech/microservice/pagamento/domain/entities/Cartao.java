ae56a0e0a75b6d1e5ce42186bb2392ad5eb9ea57
Elisa Souza <contato.elisasouza@hotmail.com>
2024-04-27 17:23:04 -0300

    [MSP-01] Atualizar entidade cartao

--- src/main/java/ada/tech/microservice/pagamento/domain/entities/Cartao.java
+++ src/main/java/ada/tech/microservice/pagamento/domain/entities/Cartao.java
@@ -19,34 +19,29 @@ import java.util.Date;
 public class Cartao {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long cartaoId;
+    @Column(name = "id_cartao", nullable = false, unique = true)
+    private String id_cartao;
 
-    @Column(unique = true)
+    @Column(name = "numero_cartao", nullable = false, unique = true)
     @NotBlank(message = "Esse campo é obrigatório!")
     @Size(min = 16, max = 16, message = "Esse campo deve conter 16 dígitos!")
-    private Integer numeroCartao;
+    private String numero_cartao;
 
     @Column
     @NotBlank(message = "Esse campo é obrigatório")
     @Size(min = 10, message = "Esse campo deve conter no mínimo 10 caracteres")
-    private String titularCartao;
+    private String titular;
 
-    @Column
+    @Column(name = "cvv", nullable = false)
     @NotBlank(message = "Esse campo é obrigatório")
     @Size(min = 3, max = 3, message = "Esse campo deve ter 3 dígitos!")
-    private Integer cvv;
+    private String cvv;
 
-    @Column
+    @Column(name = "validade_cartao", nullable = false)
     @NotBlank(message = "Esse campo é obrigatório")
     @Temporal(TemporalType.DATE)
     private Date validadeCartao;
 
-    public Cartao(Integer numeroCartao, String titularCartao, Integer cvv, Date validadeCartao) {
-        this.numeroCartao = numeroCartao;
-        this.titularCartao = titularCartao;
-        this.cvv = cvv;
-        this.validadeCartao = validadeCartao;
-    }
 
     public void setValidadeCartao(String validadeCartao) {
         try {
