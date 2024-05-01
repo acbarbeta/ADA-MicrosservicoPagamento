4b45d030416374fd8513a199283884950fe81790
Ana Clara <aclara.bvicente@gmail.com>
2024-04-30 23:10:32 -0300

    feature: creates cancel payment method

--- src/main/java/ada/tech/microservice/pagamento/repository/PagamentoRepository.java
+++ src/main/java/ada/tech/microservice/pagamento/repository/PagamentoRepository.java
@@ -8,5 +8,5 @@ import java.util.Optional;
 
 @Repository
 public interface PagamentoRepository extends JpaRepository<Pagamento, Long> {
-    Optional<Pagamento> findPagamentoById(String UUID);
+    Pagamento findPagamentoById(String UUID);
 }
